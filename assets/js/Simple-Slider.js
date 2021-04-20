6eab6ea5820dd8b0d0294e1427df7a4eea6480c4
Cristopher Rivas <cristopherjrivasc@gmail.com>
2021-04-20 19:28:05 -0400

    last

--- assets/js/Simple-Slider.js
+++ assets/js/Simple-Slider.js
@@ -1,14 +1,32 @@
-$(function(){
+$(document).ready(function(){
 
-    // Initializing the swiper plugin for the slider.
-    // Read more here: http://idangero.us/swiper/api/
-    
-    var mySwiper = new Swiper ('.swiper-container', {
-        loop: true,
-        pagination: '.swiper-pagination',
-        paginationClickable: true,
-        nextButton: '.swiper-button-next',
-        prevButton: '.swiper-button-prev'
-    });
-    
-});
+    $('.customer-logos').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        centerMode: true,
+        variableWidth: true
+      });
+            
+    /*$('.customer-logos').slick({
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 1000,
+      arrows: true,
+      dots: true,
+      pauseOnHover: false,
+      responsive: [{
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 4
+        }
+      }, {
+        breakpoint: 520,
+        settings: {
+          slidesToShow: 3
+        }
+      }]
+    });*/
+  });
